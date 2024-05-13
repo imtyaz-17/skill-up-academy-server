073aece5424ed3b03c57c94c3b50d176ed19a096
imtyaz-17 <imtyazit17017@gmail.com>
2024-05-13 22:38:07 +0600

    fake database added

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
+
+app.use(cors());
+const categories = require('./data/category.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Skill Up Academy - API running')
+})
+app.get('/course-categories', (req, res) => {
+    res.send(categories)
+})
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '00') {
+        res.send(courses);
+    } else {
+        const categoryCourses  = courses.filter(cs => cs.category_id === id);
+        res.send(categoryCourses);
+    }
+
 })
 
+app.get('/courses', (req, res) => {
+    res.send(courses);
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse  = courses.find(course => course.id === id);
+    res.send(selectedCourse );
+})
 app.listen(port, () => {
-    console.log(`SkillUp Academy - app listening on port ${port}`)
+    console.log(`news app listening on port ${port}`)
 })
